70747f112778d000970a2cb1889842ac8cf0f523
Tatev86 <davtyanttatev@gmail.com>
2022-11-28 11:12:27 -0800

    homework

--- main.js
+++ main.js
@@ -1,44 +1,43 @@
 
-//  const fname = prompt("Input Name");
-//  const lname = prompt("Input Surname");
-//  const userage = Number(prompt("Input age"));
+const fname = prompt("Input Name");
+const lname = prompt("Input Surname");
+const userage = Number(prompt("Input age"));
 
- 
- 
+const user = (firstname, lastname, age) => {
 
-//  const  user = (firstname,lastname,age)  =>{
-//     if (isNaN(userage)=== true){
-//         console.log ("Enter the Number!")
-//     }
-//    if (userage === false) {
-   
-//    do {
-//         const fname = prompt("Input Name");
-//         const lname = prompt("Input Surname");
-//         const userage = Number(prompt("Input age"));
-//         }
-//         while (userage === true)
-//     }
-//     const obj = {
-//         firstname: firstname,
-//         lastname: lastname,
-//         age: age
-//     };
-//     return obj;
- 
-//   };
+    if (isNaN(userage) === true) {
+        console.log("Enter Number!")
+    }
 
-//  const data = user(fname,lname,userage);
-//  console.log (data);
+    while (!true); {
+        Number(prompt("Input age"));
+        if (userage) {
+            break;
+        }
+    }
 
+    const obj = {
+        firstname: firstname,
+        lastname: lastname,
+        age: age
+    }
+    return obj;
 
-
-function repet (text = "Hello", count = 4){
-    let a = "";
-    for (let i = 0; i <= count; i++){
-        a += text;
-       } 
-    return a;
 }
-const rest = repet ("Hi" , 2);
-console.log (rest)
+
+const data = user(fname, lname, userage);
+console.log(data);
+
+
+
+
+
+// function repet (text = "Hello", count = 4){
+//     let a = "";
+//     for (let i = 0; i <= count; i++){
+//         a += text;
+//        }
+//     return a;
+// }
+// const rest = repet ("Hi" , 2);
+// console.log (rest)
