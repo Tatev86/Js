098794533f5e9250c4a291c9a85d70cd63daaab4
Tatev86 <davtyanttatev@gmail.com>
2022-12-03 09:18:21 -0800

    functionClousures

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
-
-const arr = [null,34, "hello", 8, 0,"",null,"test"]
-const filterByID =(arr) => {
-    const newArray =[];
-    for (let i=0; i< arr.length; i++)
-  if ((arr[i] !== null) && (arr[i] !== ""){
-      newArray.push(arr[i]);
-      }
-    return newArray;
+function compare (a){
+    return function (b){
+        if (a>b){
+            return true;
+        }
+        if (a<b){
+            return false;
+        }
+         return null;
+    }
 }
-const newArr = filterByID(arr);
-console.log (arr);
-console.log(newArr);
+let comp= compare(20);
+console.log(comp(10));
+console.log(comp(20));
+console.log(comp(35))
